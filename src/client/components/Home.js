a1d781c447a054d5dfe221671c5f113afc613360
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-06-22 09:10:39 +0530

    Send client side bundle

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div>
+			<div>Home Component page - </div>
+			<button onClick={() => console.log("Hi")}>Press me!</button>
+		</div>
+	);
+};
+
+export default Home;
